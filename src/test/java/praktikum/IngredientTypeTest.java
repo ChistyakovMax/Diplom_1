bfac0e6726ceba59122c92e31f2bda1e95f118cd
Max Chistyakov <chistyakovmaxim2000@gmail.com>
2022-11-01 22:16:49 +0300

    IngredientTest add

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,21 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTypeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void values() {
+    }
+
+    @Test
+    public void valueOf() {
+    }
+}
